b8793391cc8d5dbe6dc13f286f9ec92a11246cf3
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2025-07-04 19:35:38 +0200

    add config for pipeline, when manually run a popup will ask for config

--- dags/pipelilne_loader.py
+++ dags/pipelilne_loader.py
@@ -13,6 +13,7 @@ sys.path.append(str(steps_path))
 
 logger = logging.getLogger(__name__)
 
+
 def validate_yaml(yaml_path: str, schema_path: str):
     with open(yaml_path) as f:
         yaml_data = yaml.safe_load(f)
@@ -34,6 +35,35 @@ def load_pipeline(yaml_path: str):
         schedule_interval=config["pipeline"]["schedule"],
         start_date=datetime(2023, 1, 1),
         catchup=False,
+        params={
+            "logLevel": 'info',
+            "logFile": "app.log",
+            "outputDir": "output",
+            "outputJsonLd": "output.jsonld",
+            "outputRdf": "output.ttl",
+            "api": {
+                "baseURL": "http://localhost/api"
+            },
+            "context": {
+                "baseURI": "http://example.globalise.nl/temp",
+                "uniqueField": ["id", "@type"],
+                "stopTables": ["logentry", "permission", "group", "user", "contenttype", "session",
+                               "postgisgeometrycolumns",
+                               "postgisspatialrefsys", "places"],
+                "middleTables": ["timespan2source", "polity2source", "politylabel2source", " rulership2source",
+                                 "rulershiplabel2source", "rulergender2source", "ruler2source", "rulerlabel2source",
+                                 "reign2source", "location2countrycode", "location2coordsource", "location2source",
+                                 "location2externalid", "location2type", "locationtype2source", "locationlabel2source",
+                                 "locationpartof2source", "shiplabel2source", "ship2externalid", "ship2type",
+                                 "ship2source", "event2source", "event2location", "translocation2externalid",
+                                 "translocation2source", "translocation2location", "locationlabel"
+                                 ],
+                "mainEntryTables": ["polity", "politylabel", "reign", "ruler", "rulership", "rulershiplabel",
+                                    "rulerlabel",
+                                    "shiplabel", "location", "event", "translocation"],
+                "notSure": ["locationpartof"]
+            },
+        }
     )
 
     tasks = {}
