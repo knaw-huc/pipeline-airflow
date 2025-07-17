4050151d2c6e61972f3f5508221437d38b3f8228
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2025-07-17 22:31:50 +0200

    add json to csv sub task

---
+++ dags/pipelines/steps/JSONToCSVOperator/JSONToCSVOperator.py
@@ -0,0 +1,25 @@
+import io
+import csv
+import logging
+from airflow.models import BaseOperator
+from utils import get_step_names
+
+class JSONToCSVOperator(BaseOperator):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.logger = logging.getLogger(__name__)
+
+    def execute(self, context):
+        input_data = context['ti'].xcom_pull(task_ids=None, key='previous_output')
+        if input_data:
+            self.logger.info(f"Input data received: {input_data}")
+            otuput = io.StringIO()
+            writer = csv.DictWriter(otuput, fieldnames=input_data.keys())
+            writer.writeheader()
+            writer.writerow(input_data)
+            csv_data = otuput.getvalue()
+            context["ti"].xcom_push("previous_output", csv_data)
+            return csv_data
+        else:
+            self.logger.info("input data is None")
+            return None
